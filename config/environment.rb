44bc34261828992da7079b9f81aed65ab08d7e5e
Martin Englund <martin@englund.nu>
2010-05-22 15:12:58 +0200

    update gems

--- config/environment.rb
+++ config/environment.rb
@@ -25,7 +25,8 @@ Rails::Initializer.run do |config|
   config.gem "formtastic"
   config.gem 'will_paginate'
   config.gem 'delayed_job'
-  
+  config.gem 'jrails'
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
